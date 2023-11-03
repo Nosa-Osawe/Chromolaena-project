728421b89ad482b1d8a6d41a5084505b06e3dd4f
Nosa <osaweemmanulnosa@gmail.com>
2023-11-03 20:57:15 +0100

    data preperation

--- Code/Chromolaena.R
+++ Code/Chromolaena.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+# extract data fo pitfall trap collections 
 Ogua_PF_0 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_PF0%.csv")
 Ogua_PF_1 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_PF_less_than_50%.csv")
 Ogua_PF_2 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_PF_greater_than_50%.csv")
@@ -112,5 +113,105 @@ sum(is.na(Benin)) ### good
 ## This the corrected Benin pitfall collection data that will be used for this analysis
 write.csv(Benin, 
           file = "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\Benin_PF.csv", 
+          row.names = FALSE)
+
+###################################################################################################
+rm(list = ls()) # clear workspace 
+library(tidyverse)
+# extract data for beating tray collections
+Ogua_BT_0 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_BT0%.csv")
+Ogua_BT_1 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_BT_less_than_50%.csv")
+Ogua_BT_2 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_BT_greater_than_50%.csv")
+Igue_BT_0 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Igueosagie_BT0%.csv")
+Igue_BT_1 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Igueosagie_less_than_BT50%.csv")
+Igue_BT_2 <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\C_Benin_Ogua_BT_greater_than_50%.csv")
+
+# check missing values
+sum(is.na(Ogua_BT_0))
+sum(is.na(Ogua_BT_1))
+sum(is.na(Ogua_BT_2))
+sum(is.na(Igue_BT_0))
+sum(is.na(Igue_BT_1))
+sum(is.na(Igue_BT_2)) # All good
+
+# Add serial number 
+Ogua_BT_0$SN <- seq_along(Ogua_BT_0$ID)
+Ogua_BT_1$SN <- seq_along(Ogua_BT_1$ID)
+Ogua_BT_2$SN <- seq_along(Ogua_BT_2$ID)
+Igue_BT_0$SN <- seq_along(Igue_BT_0$ID)
+Igue_BT_1$SN <- seq_along(Igue_BT_1$ID)
+Igue_BT_2$SN <- seq_along(Igue_BT_2$ID)
+
+# add location and Chromolaena level as columns
+
+Ogua_BT_0 <- Ogua_BT_0 %>%
+  mutate(Location = "Ogua",
+         Chromolaena_level = "0%")
+
+Ogua_BT_1 <- Ogua_BT_1 %>%
+  mutate(Location = "Ogua",
+         Chromolaena_level = "<50%")
+
+Ogua_BT_2 <- Ogua_BT_2 %>%
+  mutate(Location = "Ogua",
+         Chromolaena_level = ">50%")
+Ogua_BT <- rbind(Ogua_BT_0, Ogua_BT_1, Ogua_BT_2)
+## try not to mutate ore than once, else you add more columns
+
+Igue_BT_0 <- Igue_BT_0 %>%
+  mutate(Location = "Igueosagie",
+         Chromolaena_level = "0%")
+
+Igue_BT_1 <- Igue_BT_1 %>%
+  mutate(Location = "Igueosagie",
+         Chromolaena_level = "<50%")
+
+Igue_BT_2 <- Igue_BT_2 %>%
+  mutate(Location = "Igueosagie",
+         Chromolaena_level = ">50%")
+
+Igue_BT <- rbind(Igue_BT_0, Igue_BT_1, Igue_BT_2)
+view(Ogua_BT)
+View(Igue_BT)
+
+Benin_BT <- rbind(Ogua_BT, Igue_BT)
+view(Benin_BT)
+
+# we spotted repetitions in the data which needs filtering
+
+filtered_Benin2 <- Benin_BT %>% 
+  filter(!(SN >= 438 & SN <= 500)) ## took it a bit overboard for safety
+
+Benin_BT <- filtered_Benin2
+
+# remove the SN for now since its not correct for this combined data frame
+Benin_BT <- Benin_BT %>%
+  select(-SN)
+#### lets give a new serial number (SN)
+
+Benin_BT$SN <- seq_along(Benin_BT$ID)
+Max_SN_BT <- max(Benin_BT$SN)
+# Count the number of distinct ID
+distinct_ID<- Benin_BT %>%distinct(ID) %>% count()
+
+BT_count_ID<- Benin_BT %>%
+  group_by(ID) %>%
+  summarize(count = n()) ## most should be 6
+
+# Filter rows where count is not equal to 6
+filtered_BT_count_ID <- BT_count_ID %>%
+  filter(count != 6)
+
+#  number of rows where count is not equal to 6
+rows_not_equal_to_6 <- nrow(filtered_BT_count_ID)
+
+# Print the count of rows where count is not equal to 6
+print(rows_not_equal_to_6)
+
+view(Benin_BT)
+
+## This the corrected Benin pitfall collection data that will be used for this analysis
+write.csv(Benin_BT, 
+          file = "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\Benin_BT.csv", 
           row.names = FALSE)
 
