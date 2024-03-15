b2416c89698232cac052270d6fe2ffb204ad909e
Nosa <osaweemmanulnosa@gmail.com>
2024-03-15 23:21:04 +0100

    updates

--- Code/new_.R
+++ Code/new_.R
@@ -11,6 +11,7 @@ unique(bt_all$Location)
 attach(pf_all)
 attach(bt_all)
 
+###########-----------------------------------------------------------------------------
 Ogua_pf <- pf_all %>% 
   filter(Location == "Ogua") 
 
@@ -28,6 +29,9 @@ ogua_bt_by_fam_by_invation <-ogua_bt %>%
 
 view(ogua_bt_by_fam_by_invation)
 
+length(ogua_bt_by_fam_by_invation$Family)
+length(ogua_pf_by_fam_by_invation$Family)
+
 ogua_pf_by_fam_by_invation<- Ogua_pf %>% 
   group_by(Family, Chromolaena_level) %>% 
   summarise(across(where(is.numeric), sum)) %>%
@@ -40,6 +44,55 @@ write.csv(ogua_pf_by_fam_by_invation,
 
 write.csv(ogua_bt_by_fam_by_invation,
           "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\ogua_bt_fam.csv")
+
+ogua_c<- merge(ogua_bt_by_fam_by_invation, ogua_pf_by_fam_by_invation,
+            by = c("Family","Chromolaena_level"))
+ogua_c <- as.data.frame(ogua_c)
+view(ogua_c)
+
+write.csv(ogua_c, "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\ogua_combined.csv")
+
+
+
+###########-----------------------------------------------------------------------------
+
+Igueosagie_pf <- pf_all %>% 
+  filter(Location == "Igueosagie") 
+
+length(Igueosagie_pf$Location)
+
+Igueosagie_bt <- bt_all %>% 
+  filter(Location=="Igueosagie")
+
+length(Igueosagie_bt$Location)
+
+Igueosagie_bt_by_fam_by_invation <-Igueosagie_bt %>% 
+  group_by(Family,Chromolaena_level) %>% 
+  summarise(across(where(is.numeric), sum)) %>% 
+  select(-SN)
+
+view(Igueosagie_bt_by_fam_by_invation)
+
+length(Igueosagie_bt_by_fam_by_invation$Family)
+
+Igueosagie_pf_by_fam_by_invation<- Igueosagie_pf %>% 
+  group_by(Family, Chromolaena_level) %>% 
+  summarise(across(where(is.numeric), sum)) %>%
+  select(-SN)
+length(Igueosagie_pf_by_fam_by_invation$Family)
+
+
+Igueosagie_c<- merge(Igueosagie_bt_by_fam_by_invation,
+                     Igueosagie_pf_by_fam_by_invation,
+               by = c("Family","Chromolaena_level"))
+Igueosagie_c <- as.data.frame(Igueosagie_c)
+view(Igueosagie_c)
+
+write.csv(Igueosagie_c, 
+          "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\Igueosagie_combined.csv")
+
+
+
 
 
 
