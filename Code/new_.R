a1c534c30828212c3ad80747b0152264d45c8976
Nosa <osaweemmanulnosa@gmail.com>
2024-03-15 17:42:39 +0100

    updates

---
+++ Code/new_.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+bt_all <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\Benin_BT.csv")
+pf_all <- read.csv("C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\Benin_PF.csv")
+
+view(pf_all)
+view(bt_all)
+
+unique(bt_all$Location)
+
+attach(pf_all)
+attach(bt_all)
+
+Ogua_pf <- pf_all %>% 
+  filter(Location == "Ogua") 
+
+length(Ogua_pf$Location)
+
+ogua_bt <- bt_all %>% 
+  filter(Location=="Ogua")
+
+head(ogua_bt, 5)
+
+ogua_bt_by_fam_by_invation <-ogua_bt %>% 
+  group_by(Family,Chromolaena_level) %>% 
+  summarise(across(where(is.numeric), sum)) %>% 
+  select(-SN)
+
+view(ogua_bt_by_fam_by_invation)
+
+ogua_pf_by_fam_by_invation<- Ogua_pf %>% 
+  group_by(Family, Chromolaena_level) %>% 
+  summarise(across(where(is.numeric), sum)) %>%
+  select(-SN)
+
+view(ogua_pf_by_fam_by_invation)
+
+write.csv(ogua_pf_by_fam_by_invation,
+          "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\ogua_pf_fam.csv")
+
+write.csv(ogua_pf_by_fam_by_invation,
+          "C:\\Users\\HP\\Documents\\Chromolaena-project\\Data\\ogua_pf_fam.csv")
